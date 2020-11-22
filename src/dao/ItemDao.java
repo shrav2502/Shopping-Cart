6b12bb3acb8b3fb9c76e80d6b6ec537c829821ea
shrav2502 <hebbarshravanthi@gmail.com>
2020-11-22 20:48:08 +0530

    methods added

--- src/dao/ItemDao.java
+++ src/dao/ItemDao.java
@@ -1,5 +1,35 @@
 package dao;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import bean.Item;
+import resource.DbConnection;
+
 
 public class ItemDao {
-
+	Statement stmt;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	Connection con;
+	public List<Item> retrieveAllProduct(){
+		List<Item> listofItem=new ArrayList<Item>();
+		try {
+			con=DbConnection.getDbConnection();
+			pstmt=con.prepareStatement("select * from item");
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+			Item ii=new Item();
+			ii.setItemId(rs.getString(1));
+			ii.setItemName(rs.getString(2));
+			listofItem.add(ii);
+			}
+		    }catch(Exception e) {
+		    	System.out.println("retriveAll Item"+e);
+		}
+		return listofItem;
+		}
 }
--- src/dao/ProductDao.java
+++ src/dao/ProductDao.java
@@ -1,5 +1,35 @@
 package dao;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import bean.Product;
+import resource.DbConnection;
 
 public class ProductDao {
-
+	Statement stmt;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	Connection con;
+	public List<Product> retrieveAllProduct(){
+		List<Product> listofProduct=new ArrayList<Product>();
+		try {
+			con=DbConnection.getDbConnection();
+			pstmt=con.prepareStatement("select * from product");
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+			Product pp=new Product();
+			pp.setProductId(rs.getString(1));
+			pp.setProductName(rs.getString(2));
+			pp.setItemId(rs.getString(3));
+			pp.setPrice(rs.getInt(4));
+			}
+		    }catch(Exception e) {
+		    	System.out.println("retriveAll Product"+e);
+		}
+		return listofProduct;
+		}
 }
